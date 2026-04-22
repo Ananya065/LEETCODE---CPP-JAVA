dd3775fa07728785c8177563d1be43d07b9c32be
Ananya Mishra <2k24.cs1c.2410091@gmail.com>
2026-04-23 00:02:26 +0530

    Update 22_04_2026.cpp

--- 22_04_2026.cpp
+++ 22_04_2026.cpp
@@ -1,1 +1,29 @@
+class Solution {
+public:
+    vector<string> twoEditWords(vector<string>& queries, vector<string>& dictionary) {
+        vector<string> result;
 
+        for (const string& q : queries) {
+            bool found = false;
+            for (const string& d : dictionary) {
+                int edits = 0;
+                for (int i = 0; i < q.length(); ++i) {
+                    if (q[i] != d[i]) {
+                        edits++;
+                    }
+                    if (edits > 2) break;
+                }
+
+                if (edits <= 2) {
+                    found = true;
+                    break;
+                }
+            }
+            if (found) {
+                result.push_back(q);
+            }
+        }
+
+        return result;
+    }
+};
