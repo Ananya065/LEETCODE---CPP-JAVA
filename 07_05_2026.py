14242ba7a508c3572ddd448e42169434e4faf287
Ananya Mishra <2k24.cs1c.2410091@gmail.com>
2026-05-07 21:21:24 +0530

    Implement maxValue function to compute max values

---
+++ 07_05_2026.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxValue(self, nums: list[int]) -> list[int]:
+        n = len(nums)
+        if n == 0:
+            return []
+
+        pre_max = [0] * n
+        pre_max[0] = nums[0]
+        for i in range(1, n):
+            pre_max[i] = max(pre_max[i - 1], nums[i])
+
+        ans = [0] * n
+        suf_min = float('inf')
+        
+        for i in range(n - 1, -1, -1):
+            if pre_max[i] > suf_min:
+                ans[i] = ans[i + 1]
+            else:
+                ans[i] = pre_max[i]
+            
+            if nums[i] < suf_min:
+                suf_min = nums[i]
+                
+        return ans
