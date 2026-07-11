73edc15a1b594e59b0198eb45522af86898609f1
Ananya Mishra <2k24.cs1c.2410091@gmail.com>
2026-07-11 21:58:09 +0530

    Time: 62 ms (35.92%), Space: 137.5 MB (26.91%) - LeetHub

---
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
@@ -0,0 +1,50 @@
+#include <vector>
+#include <queue>
+
+class Solution {
+public:
+    int countCompleteComponents(int n, std::vector<std::vector<int>>& edges) {
+        std::vector<std::vector<int>> adj(n);
+        std::vector<int> degree(n, 0);
+        for (const auto& edge : edges) {
+            adj[edge[0]].push_back(edge[1]);
+            adj[edge[1]].push_back(edge[0]);
+            degree[edge[0]]++;
+            degree[edge[1]]++;
+        }
+
+        std::vector<bool> visited(n, false);
+        int completeComponents = 0;
+
+        for (int i = 0; i < n; ++i) {
+            if (!visited[i]) {
+                int vertexCount = 0;
+                int totalDegree = 0;
+                std::queue<int> q;
+
+                q.push(i);
+                visited[i] = true;
+
+                while (!q.empty()) {
+                    int curr = q.front();
+                    q.pop();
+                    vertexCount++;
+                    totalDegree += degree[curr];
+
+                    for (int neighbor : adj[curr]) {
+                        if (!visited[neighbor]) {
+                            visited[neighbor] = true;
+                            q.push(neighbor);
+                        }
+                    }
+                }
+
+                if (totalDegree == vertexCount * (vertexCount - 1)) {
+                    completeComponents++;
+                }
+            }
+        }
+
+        return completeComponents;
+    }
+};
